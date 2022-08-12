dfe2c0402eb238f410f177721959ec10ddbd9b11
Alzat <admin@eco-trest.ru>
2022-08-12 15:14:38 +0300

    edit Annotate2

--- Annotate2.py
+++ Annotate2.py
@@ -1,4 +1,7 @@
-def lower_join(seq):
+from typing import Sequence, Dict, List, Tuple, Set
+
+
+def lower_join(seq: Sequence [str]) -> str:
     """Принимает на вход последовательность и создаёт из неё  
     строку в нижнем регистре."""
     return ''.join(seq).lower()
